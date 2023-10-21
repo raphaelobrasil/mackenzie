19d6ccbb9c84006830e036e78f5261c61e0dbbd5
Raphael Brasil <raphael.brasil>
2023-10-21 13:13:30 -0300

    feat: update favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 export default function Home() {
   return (
-    <div className="relative flex w-full flex-col p-1 py-4">
-      <div className="flex min-h-[70vh] flex-1 rounded-xl bg-red-m p-12 py-6">
+    <div className="relative flex w-full flex-col  p-1 py-4 max-md:p-0 max-md:py-0">
+      <div className="flex min-h-[70vh] flex-1 rounded-xl bg-red-m p-12 py-6 max-md:rounded-none">
         <aside>MENU</aside>
         <h3>TITULO</h3>
       </div>
-      <main className="top-45 absolute left-1/2">
+      <main className="absolute left-1/2 top-45">
         <div>Card 1</div>
         <div>Card 2</div>
         <div>Card 3</div>
